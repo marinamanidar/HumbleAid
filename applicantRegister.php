adcc65ac093078ce6ca8f67e4c2e9f024115976e
marinamanidar <marinamanidar13@gmail.com>
2022-03-05 17:08:37 +0800

    commit

--- applicantRegister.php
+++ applicantRegister.php
@@ -8,10 +8,73 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Humble Aid</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+  <nav class="navbar fixed-top" style="background-color: #507daf;">
+  <div class="container-fluid">
+    <a class="navbar-brand" >Humble Aid </a>
+    <form class="d-flex">
+        <p style="padding: 0px 50px;">John</p>
+      <button class="btn" type="submit">Log Out</button>
+    </form>
+  </div>
+</nav>
+
+  <section class="vh-100 bg-image">
+    <div class="mask d-flex align-items-center h-100 gradient-custom-3" style="padding-top: 100px;">
+      <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+            <div class="card" style="border-radius: 15px;">
+              <div class="card-body p-5">
+                <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+
+                <form>
+
+                  <div class="form-outline mb-4">
+                    <input
+                    type="text"
+                    class="form-control"
+                    placeholder="John Doe"
+                    aria-label="Full Name"
+                    aria-describedby="basic-addon1"
+                  />
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="text" id="typeText" class="form-control" placeholder="123456-01-1254"/>
+                    <label class="form-label" for="typeText"></label>
+                  </div>
+
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">+60</span>
+                    </div>
+                    <input type="text" id="typeText" class="form-control" placeholder="123345622" />
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                    <label class="form-label" for="form3Example4cdg">Repeat your password</label>
+                  </div>
+
+                  <div class="d-flex justify-content-center">
+                    <button type="button" class="btn btn-success btn-block btn-lg gradient-custom-4 text-body" style="background-color: #507daf;">Register</button>
+                  </div>
+
+                  <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!" class="fw-bold text-body"><u>Login here</u></a></p>
+
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
